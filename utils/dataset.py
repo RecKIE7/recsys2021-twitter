9820d5cf44a0499ba9320083172f88b97bc149ae
HYEZ <oohyejioo@gmail.com>
2021-03-27 18:48:50 +0900

    update engagement analysis

--- utils/dataset.py
+++ utils/dataset.py
@@ -3,15 +3,19 @@ from dask.distributed import Client, wait, progress
 from dask_cuda import LocalCUDACluster
 from utils.cuda_cluster import client
 
-def read_data(path, type='parquet'):
+def read_data(path, type='parquet', index=False):
 
 
     if type == 'parquet':
-        df = dask_cudf.read_parquet(f'{path}/*.parquet')
+        df = dask_cudf.read_parquet(f'{path}/*.parquet', index=False)
         df, = dask.persist(df)
+        df = df.set_index('id', drop=True)
         _ = wait(df)
         print('number of rows:',len(df)) 
         return df
     else:
         print('cannot read data')
 
+
+def arr_to_series(arr):
+    return dask_cudf.Seriese(arr)
