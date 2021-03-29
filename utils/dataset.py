6516ec545ce270e427e5f71884bdfa5d8b7e0179
HYEZ <oohyejioo@gmail.com>
2021-03-29 14:35:17 +0900

    check the number of unique users, creators, and engagers

--- utils/dataset.py
+++ utils/dataset.py
@@ -16,13 +16,20 @@ def read_data(path, type='parquet', index=False):
 
 
 
-def factorize_small_cardinality(df, col):
+def factorize_small_cardinality(df, col, tmp=None, is_several=False):
     df['id'] = df.index
     tmp_col = f'{col}_encode'
+
+    if not is_several:
+        tmp = df[col].unique().compute()
     
-    tmp = df[col].unique().compute()
+
     idx_to_col = dict(zip(tmp.index.to_array(), tmp.to_array()))
     tmp = tmp.to_frame().reset_index()
+
+    if is_several:
+        tmp[col] = tmp[0]
+        tmp = tmp.drop(0, axis=1)
     
     df = df.merge(tmp,on=col,how='left')
     df, = dask.persist(df)
@@ -35,3 +42,25 @@ def factorize_small_cardinality(df, col):
     df, = dask.persist(df)
     wait(df)
     return df, idx_to_col
+
+def map_features_to_dict(df, col, sereis):
+    df['id'] = df.index
+    tmp_col = f'{col}_encode'
+
+    tmp = sereis
+    idx_to_col = dict(zip(tmp.index.to_array(), tmp.to_array()))
+    tmp = tmp.to_frame().reset_index()
+    
+    
+    df = df.merge(tmp,on=col,how='left')
+    df, = dask.persist(df)
+    wait(df)
+    del tmp
+
+    df[tmp_col] = df['index']
+    df = df.drop('index',axis=1)
+    df = df.set_index('id', drop=True)
+    df, = dask.persist(df)
+    wait(df)
+    return df, idx_to_col
+    
