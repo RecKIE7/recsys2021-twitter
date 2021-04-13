c3b80144e24582fbff4b13bc9a9ac5972593967c
HYEZ <oohyejioo@gmail.com>
2021-04-13 20:01:08 +0900

    add Dataset class : processing all data

--- core/config.py
+++ core/config.py
@@ -4,13 +4,10 @@ project_root = '~/kie/recsys2021-twitter/'
 raw_data_path = data_root + 'raw/'
 submission_path = project_root + 'submission/'
 preproc_path = '/hdd/preprocessing/'
+raw_lzo_path = '/hdd/twitter/raw_lzo/'
 
 # features
-raw_features = ["text_tokens", "hashtags", "tweet_id", "present_media", "present_links", "present_domains",\
-                "tweet_type","language", "tweet_timestamp", "engaged_with_user_id", "engaged_with_user_follower_count",\
-               "engaged_with_user_following_count", "engaged_with_user_is_verified", "engaged_with_user_account_creation",\
-               "engaging_user_id", "enaging_user_follower_count", "enaging_user_following_count", "enaging_user_is_verified",\
-               "enaging_user_account_creation", "engagee_follows_engager"]
+raw_features = ["text_tokens", "hashtags", "tweet_id", "media", "links", "domains", "tweet_type","language", "tweet_timestamp", "creator_id", "creator_follower_count", "creator_following_count", "creator_is_verified", "creator_account_creation","engager_id", "engager_follower_count", "engager_following_count", "engager_is_verified", "engager_account_creation", "creator_follows_engager"]
 labels = ["reply_timestamp", "retweet_timestamp", "retweet_with_comment_timestamp", "like_timestamp"]
 
 # parameters
--- train.py
+++ train.py
@@ -1,34 +1,15 @@
 import os
-
-from models.baseline.random_model import random_prediction_model
-from core.config import raw_features
-import core.config as conf
-from tqdm import tqdm
-
-all_features_to_idx = dict(zip(raw_features, range(len(raw_features))))
-
-def parse_input_line(line):
-    features = line.split("\x01")
-    tweet_id = features[all_features_to_idx['tweet_id']]
-    user_id = features[all_features_to_idx['engaging_user_id']]
-    input_feats = features[all_features_to_idx['text_tokens']]
-    return tweet_id, user_id, input_feats
+import fire
 
 
-def evaluate_test_set():
-    model = random_prediction_model
-    path = '/hdd/twitter/raw_lzo/'
-    part_files = [os.path.join(path, f) for f in os.listdir(path) if 'part' in f]
-    with open('submission/results.csv', 'w') as output:
-        for file in tqdm(part_files):
-            with open(file, 'r') as f:
-                for line in f.readlines():
-                    tweet_id, user_id, features = parse_input_line(line)
-                    reply_pred = model(features)
-                    retweet_pred = model(features)
-                    quote_pred = model(features)
-                    fav_pred = model(features)
-                    output.write(f'{tweet_id},{user_id},{reply_pred},{retweet_pred},{quote_pred},{fav_pred}\n')
+class Train(object):
+    def __init__(self):
+        self.dataset = Dataset(training_flag=True)
+        
+
+
 
 if __name__ == "__main__":
-    evaluate_test_set()
+    from utils.dataset import *
+
+    fire.Fire(Train)
