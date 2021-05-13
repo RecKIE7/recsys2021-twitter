3659f62cdeb6815cd2b12de4ffa3df7bd1c3f4ba
HYEZ <oohyejioo@gmail.com>
2021-05-13 10:44:50 +0900

    submit

--- models/model/XGBoost.py
+++ models/model/XGBoost.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 import pandas as pd, numpy as np, gc
 from tqdm import tqdm
 import joblib
-import cudf, cupy, time
 import xgboost as xgb
 
 pd.set_option('display.max_columns', 500)
