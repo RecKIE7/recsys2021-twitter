ca7673c287e9a7b6cbdbd9a3e1259474d4958beb
HYEZ <oohyejioo@gmail.com>
2021-05-17 15:11:13 +0900

    autoencoder + tweet features

--- core/config.py
+++ core/config.py
@@ -41,4 +41,4 @@ REPLY = 0
 RETWEET = 1
 COMMNET = 2
 LIKE = 3
-target_encoding = 2 # run target encoding? (0:False, 1:simple_encoder, 2:MTE_encoder)
+target_encoding = 0 # run target encoding? (0:False, 1:simple_encoder, 2:MTE_encoder)
